f304b5530bbd77a8bed7cea608350969b08559c8
Ishtabalern <105914404+Ishtabalern@users.noreply.github.com>
2023-10-24 21:53:57 +0800

    Add files via upload

--- weatherapp.py
+++ weatherapp.py
@@ -1,7 +1,8 @@
 import requests
+import pytz
+from datetime import datetime
 
-
-def get_weather(city, api_key):
+def get_weather(city, api_key, timezone):
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     params = {
         "q": city,
@@ -19,14 +20,19 @@ def get_weather(city, api_key):
         temperature = main_data["temp"]
         description = weather_data["description"]
 
+        current_time = datetime.now(pytz.timezone(timezone))
+
         print(f"Weather in {city}:")
         print(f"Temperature: {temperature}°C")
         print(f"Description: {description.capitalize()}")
+        print(f"Current Time: {current_time.strftime('%Y-%m-%d %H:%M:%S')} ({timezone})")
     else:
         print(f"Could not fetch weather data for {city}. Error code: {data['cod']}")
 
 while True:
     if __name__ == "__main__":
         api_key = "3301007ff78e08c345eeba4bc785d345"  # Replace with your OpenWeatherMap API key
+        continent = input("Enter a Continent: ")
         city = input("Enter a city: ")
-        get_weather(city, api_key)
+        timezone = str(continent + "/" + city)
+        get_weather(city, api_key, timezone)
